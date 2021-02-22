9c17b49a98c8f457636c80e38c88a2b4cc762bac
Adarsh Singh <adarshkumar@bitpod.io>
2021-02-22 08:00:19 +0000

    chattab for reff added

--- react/features/chat/components/web/ChatTab.js
+++ react/features/chat/components/web/ChatTab.js
@@ -2,15 +2,14 @@ import React from 'react';
 
 import Tabs from '@atlaskit/tabs';
 import ChatIframe from './ChatIframe';
-// import Chat from './Chat';
+import Chat from './Chat';
 
 
-// const ChatView = () => <Chat/>;
+const ChatView = () => <Chat/>;
 
 const tabs = [
-  // { label: 'Chat', content: <ChatView />},
-  { label: 'Tab 1', content: 'checkview'},
-  { label: 'Tab 2', content: <ChatIframe />},
+  { label: 'Backstage', content: <ChatView />},
+  { label: 'Live', content: <ChatIframe />},
 ];
 
 const ChatTab = () => <Tabs tabs={tabs} />;
